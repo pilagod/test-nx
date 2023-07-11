7a87b77649d3eb3b5d99639fc746ed8d3d35e725
pilagod <pilagooood@gmail.com>
2023-07-11 21:09:29 +0800

    add oz dependency

--- packages/foundry/src/Counter.sol
+++ packages/foundry/src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
+
 contract Counter {
     uint256 public number;
 
@@ -10,5 +12,9 @@ contract Counter {
 
     function increment() public {
         number++;
+    }
+
+    function getBalance(IERC20 token, address owner) public view returns (uint256) {
+        return token.balanceOf(owner);
     }
 }
