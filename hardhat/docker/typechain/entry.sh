eaaba87566b1b853f2f63bc2e6b37241d850ec18
pilagod <pilagooood@gmail.com>
2023-07-12 15:51:18 +0800

    export hardhat artifacts from docker

--- hardhat/docker/typechain/entry.sh
+++ hardhat/docker/typechain/entry.sh
@@ -2,6 +2,17 @@
 
 source ~/.bashrc
 npx hardhat typechain
+
+if [ -d "/out/artifacts" ]; then
+    rm -rf /out/artifacts
+fi
+mv ./artifacts /out
+
+if [ -d "/out/cache" ]; then
+    rm -rf /out/cache
+fi
+mv ./cache /out
+
 if [ -d "/out/typechain-types" ]; then
     rm -rf /out/typechain-types
 fi
---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,6 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+
+export default {
+  solidity: "0.8.17",
+};
