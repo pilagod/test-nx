bddcf4dc650fb30ef9d1b5fbd67becfd639740d3
pilagod <pilagooood@gmail.com>
2023-07-13 14:34:01 +0800

    update entry script

--- hardhat/docker/build/entry.sh
+++ hardhat/docker/build/entry.sh
@@ -5,17 +5,9 @@ source ~/.bashrc
 npx hardhat compile --no-typechain
 npx hardhat typechain
 
-if [ -d "./artifacts" ]; then
-    rm -rf /out/artifacts
-    mv ./artifacts /out
-fi
+rm -rf /out/artifacts /out/cache /out/typechain-types
 
+mv ./artifacts ./typechain-types /out
 if [ -d "./cache" ]; then
-    rm -rf /out/cache
     mv ./cache /out
-fi
-
-if [ -d "./typechain-types" ]; then
-    rm -rf /out/typechain-types
-    mv ./typechain-types /out
 fi
